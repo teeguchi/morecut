c9e3bce2cfc9c779a15c3ba574a88bca283dc4b5
Takashi <takasin@ncn-k.net>
2020-02-10 16:51:56 +0900

    Added support for surrogate pairs

--- more-cut.1.2.js
+++ more-cut.1.2.js
@@ -1,37 +1,11 @@
-/*
- * moreCUT v1.2
+/**
+ * moreCUT v1.2.2
  *
- * Date: 2017-06-03
- * 
- * TERMS OF USE - moreCUT v1.2
- * 
- * Open source under the BSD License. 
- * 
- * Copyright 2017 Takashi Yamaguchi
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- * 
- * 1.Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- * 
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
- * WARRANTIES,INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT,INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ * Copyright 2020, TeeGuchi
+ *
+ * Released under the MIT License.
+ * https://github.com/teeguchi/morecut/blob/master/LICENSE
  */
-
-
 function moreCUT(c,s) {
 	
 	var className = c; // クラスを付ける
@@ -73,13 +47,22 @@ function moreCut() {
 	/* 文字列を切りつめる関数 */
 	function strCut(str,ele) {
 		for (var j = 0; j < str.length; j++) {
-			var textSlice = str.slice(0,j) + strMore;  // ...を付け加える
-			var textWidth = strWidth(textSlice,ele);
+			var textSlice = str.slice(0,j);
+			var textTotal = textSlice + strMore;  // ...を付け加える
+			var textWidth = strWidth(textTotal,ele);
 			if (textWidth <= moreCutOwidth) {
-				var textIn = textSlice;
+				var lastStr = str[j - 1];
+				var beyoStr = str[j];
+				var textCalc = textSlice;
 			}
 		}
-		return textIn;
+		// サロゲートペアの処理
+		if (lastStr != undefined) {
+			if (lastStr.search(/[\uD800-\uDBFF]/) != -1) {
+				textCalc += beyoStr;
+			}
+		}
+		return textCalc + strMore;
 		
 		/* 文字列の長さを出す処理 */
 		function strWidth(text,ele) {
